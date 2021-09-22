1f32e71fcb33803cff5c8c6326efb005a3a1edca
Jack <jack.center@picknik.ai>
2021-09-22 11:37:05 -0600

    added new default and error environments as well as a function to setup the bashrc file

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,16 +8,14 @@ if [ $BASHRC_ENV == "jack-ThinkPad-T15-Gen-1" ]; then
 
     # source /opt/ros/noetic/setup.bash
     source /opt/ros/foxy/setup.bash
-fi
 
-if [ $BASHRC_ENV == "ros2_lxc" ]; then
+elif [ $BASHRC_ENV == "ros2_lxc" ]; then
     EDITOR='code'
     export COLOR_0="0;33m"
     export COLOR_1="0;32m"
     export COLOR_2="0;0m"
-fi
 
-if [ $BASHRC_ENV == "appliedlogix" ]; then
+elif [ $BASHRC_ENV == "appliedlogix" ]; then
     EDITOR='code'
     export COLOR_0="0;31m"
     export COLOR_1="0;30m"
@@ -28,9 +26,8 @@ if [ $BASHRC_ENV == "appliedlogix" ]; then
     source ~/ws_appliedlogix/devel/setup.bash
 
     cd ~/ws_appliedlogix
-fi
 
-if [ $BASHRC_ENV == "ros2_foxy" ]; then
+elif [ $BASHRC_ENV == "ros2_foxy" ]; then
     EDITOR='code'
     export COLOR_0="0;33m"
     export COLOR_1="0;32m"
@@ -38,4 +35,30 @@ if [ $BASHRC_ENV == "ros2_foxy" ]; then
 
     # source /opt/ros/noetic/setup.bash
     source /opt/ros/foxy/setup.bash
+
+elif [ $BASHRC_ENV == "default" ]; then
+    EDITOR='code'
+    export COLOR_0="0;34m"
+    export COLOR_1="0;30m"
+    export COLOR_2="0;0m"
+
+else
+    echo "Warning: BASHRC_ENV variable defined in '~/.bashrc' not found in 'bash_shortcuts/scripts/env.sh'"
+    echo "Consider setting the BASHRC_ENV variable to your desired settings."
+
+    export EDITOR='code'
+    export COLOR_0="0;34m"
+    export COLOR_1="0;30m"
+    export COLOR_2="0;0m"
 fi
+
+# Replaces ~/.bashrc with the user defined BASHRC_ENV and BASHRC_NAME variables and adds the sourceing 
+# for the bash_shortcuts/.my.bashrc. This should be run after 
+function setup_bash 
+{
+    local ENV_VAL=${1:-none}
+    local NAME_VAL=${2:-none}
+    echo "export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL && . ~/bash_shortcuts/.my.bashrc" > ~/.bashrc
+    source ~/.bashrc
+}
+   
