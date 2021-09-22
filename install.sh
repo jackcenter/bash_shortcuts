bae070aa0364e7b6d9c90fb5f9689b397dbd93ec
Jack <jack.center@picknik.ai>
2021-09-22 13:08:53 -0600

    first commit -made executable

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Replaces ~/.bashrc with the user defined BASHRC_ENV and BASHRC_NAME variables and adds the sourceing 
+# for the bash_shortcuts/.my.bashrc. This should be run after 
+function setup_bash 
+{
+    local ENV_VAL=${1:-none}
+    local NAME_VAL=${2:-none}
+    echo "export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL && . ~/bash_shortcuts/.my.bashrc" > ~/.bashrc
+    source ~/.bashrc
+}
+
+setup_bash $1 $2
