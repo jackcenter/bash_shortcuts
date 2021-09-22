d6208d716b88d437578684d898a74a0641ec9ec6
Jack <jack.center@picknik.ai>
2021-09-22 13:09:28 -0600

    moved setup into install

--- scripts/env.sh
+++ scripts/env.sh
@@ -51,14 +51,3 @@ else
     export COLOR_1="0;30m"
     export COLOR_2="0;0m"
 fi
-
-# Replaces ~/.bashrc with the user defined BASHRC_ENV and BASHRC_NAME variables and adds the sourceing 
-# for the bash_shortcuts/.my.bashrc. This should be run after 
-function setup_bash 
-{
-    local ENV_VAL=${1:-none}
-    local NAME_VAL=${2:-none}
-    echo "export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL && . ~/bash_shortcuts/.my.bashrc" > ~/.bashrc
-    source ~/.bashrc
-}
-   
