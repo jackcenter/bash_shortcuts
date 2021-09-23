962f6f10cfe157e91646b3685702dcfd520e0baa
Jack Center <jack.d.center@gmail.com>
2021-09-23 12:08:24 -0600

    added device to environment variables

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@
 # for the bash_shortcuts/.my.bashrc.
 function setup_bash 
 {
-    local ENV_VAL=${1:-none}
-    local NAME_VAL=${2:-none}
-    echo "export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL && . ~/bash_shortcuts/.my.bashrc" > ~/.bashrc
+    local DEVICE_VAL=${1:-none}
+    local ENV_VAL=${2:-none}
+    local NAME_VAL=${3:-none}
+    echo "export BASHRC_DEVICE=$DEVICE_VAL export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL" > ~/.bashrc
+    echo ". ~/bash_shortcuts/.my.bashrc" >> ~/.bashrc 
     source ~/.bashrc
 }
 
