9d0bd9037ed6e953f8b93e1b45b8e5c21b41e95d
Jack Center <jack.d.center@gmail.com>
2021-09-23 12:15:29 -0600

    fixed export typo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ function setup_bash
     local DEVICE_VAL=${1:-none}
     local ENV_VAL=${2:-none}
     local NAME_VAL=${3:-none}
-    echo "export BASHRC_DEVICE=$DEVICE_VAL export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL" > ~/.bashrc
+    echo "export BASHRC_DEVICE=$DEVICE_VAL && export BASHRC_ENV=$ENV_VAL && export BASHRC_NAME=$NAME_VAL" > ~/.bashrc
     echo ". ~/bash_shortcuts/.my.bashrc" >> ~/.bashrc 
     source ~/.bashrc
 }
