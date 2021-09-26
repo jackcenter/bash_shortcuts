1a0d5820763fd6886057794b7d0388863bc14111
Jack Center <jack.d.center@gmail.com>
2021-09-26 10:00:40 -0600

    added lxc_setup command

--- scripts/lxc.sh
+++ scripts/lxc.sh
@@ -10,9 +10,29 @@ function lxc_open
 
   if [ "$lxc_status" != ""$1",RUNNING" ]; then
     lxc start "$1"
-    sleep ls
+    sleep 1s
   fi
 
   clear
   lxc exec "$1" -- sudo --user ubuntu --login
+}
+
+function lxc_setup
+{
+  local CONTAINER_NAME=${1:-unnammed}
+
+  # Create and start the container
+  lxc launch --profile default --profile gui ubuntu:18.04 ${CONTAINER_NAME} && wait
+
+  # Copy local ssh key to the container
+  lxc file push ~/.ssh/id_ed25519.pub ${CONTAINER_NAME}/home/ubuntu/.ssh/authorized_keys
+  lxc file push ~/.ssh/id_ed25519.pub ${CONTAINER_NAME}/home/ubuntu/.ssh
+  lxc file push ~/.ssh/id_ed25519 ${CONTAINER_NAME}/home/ubuntu/.ssh
+
+  # TODO: setup .bashrc from here
+  # 1. clone bash_shortcuts
+  # 2. create a temp file for .bashrc
+  # 3. push the temp file to the container .bashrc
+
+  lxc_open ${CONTAINER_NAME}
 }
