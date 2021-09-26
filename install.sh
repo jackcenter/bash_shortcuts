c26603d728c6bfa5a91d50270678b01b144311cd
Jack Center <jack.d.center@gmail.com>
2021-09-26 10:02:01 -0600

    started a melodic install script

--- install.sh
+++ install.sh
@@ -12,4 +12,32 @@ function setup_bash
     source ~/.bashrc
 }
 
+# Installs ros melodic.
+# 
+function install_melodic
+{
+    # TODO: check if the local install is 18.04
+
+    # Setup your computer to accept software from packages.ros.org
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    
+    # Setup keys
+    sudo apt install curl # if you haven't already installed curl
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+    # Install full
+    sudo apt update
+    sudo apt install ros-melodic-desktop-full
+
+    # Source melodic
+    source /opt/ros/melodic/setup.bash
+
+    # Install dependencies
+    sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+    sudo apt install python-rosdep
+    sudo rosdep init
+    rosdep update
+}
+
 setup_bash $1 $2 $3
